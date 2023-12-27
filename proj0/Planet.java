4c7d42ab6df7d35c316b7260f683c7a1f4949720
bignosecss <bignosecss@gmail.com>
2023-12-27 21:03:48 +0800

    Completed Planet Constructor and calcDistance method of project0!

---
+++ proj0/Planet.java
@@ -0,0 +1,36 @@
+public class Planet {
+
+    public double xxPos;
+    public double yyPos;
+    public double xxVel;
+    public double yyVel;
+    public double mass;
+    public String imgFileName;
+
+    public Planet(double xP, double yP, double xV, 
+                double yV, double m, String img) {
+        this.xxPos = xP;
+        this.yyPos = yP;
+        this.xxVel = xV;
+        this.yyVel = yV;
+        this.mass = m;
+        this.imgFileName = img;
+    }
+
+    public Planet(Planet b) {
+        this.xxPos = b.xxPos;
+        this.yyPos = b.yyPos;
+        this.xxVel = b.xxVel;
+        this.yyVel = b.yyVel;
+        this.mass = b.mass;
+        this.imgFileName = b.imgFileName;
+    }
+
+    public double calcDistance(Planet b) {
+        double dx = this.xxPos - b.xxPos;
+        double dy = this.yyPos - b.yyPos;
+
+        return Math.sqrt((Math.pow(dx, 2) + Math.pow(dy, 2)));
+    }
+
+}
